b705bf60363076ec0e9f3695c8f998cd0030ed9f
CayoLopes <cayocesarlopes@gmail.com>
2025-01-16 20:47:01 -0300

    Adicionar receita7 no diretório Unidade2

---
+++ Unidade2/receita7/app/novarota/page.js
@@ -0,0 +1,11 @@
+// app/novarota/page.js
+import { MariaPrea } from "./componentes";
+
+export default function NovaRotaHome() {
+  return (
+    <div>
+      <h1>Nova Rota, Nova Página</h1>
+      <MariaPrea mensagem="Morreu Maria Preá, mas a risada ficou!" />
+    </div>
+  );
+}
